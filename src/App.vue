ecaef9e691fe2722c52ecb2a2524f724ce45b11c
Justin <88294592+dev-justin@users.noreply.github.com>
2023-01-14 08:59:39 -0500

    Added global footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from "@/components/global/Header.vue";
+import Footer from "@/components/global/Footer.vue";
 import AuthForms from "@/components/forms/AuthForms.vue";
 </script>
 
 <template>
   <Header />
   <router-view />
+  <Footer />
   <AuthForms />
 </template>
