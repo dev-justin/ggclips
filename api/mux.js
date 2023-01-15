2b684375f7f4f4201624f998491ccfdfd4b8bf35
Justin <88294592+dev-justin@users.noreply.github.com>
2023-01-15 12:35:05 -0500

    Mux api testing

--- api/mux.js
+++ api/mux.js
@@ -1,4 +1,24 @@
 // This is deployed as a serverless function on Vercel
-export default function handler(request, response) {
-  return response.json({ query: request.query, body: request.body });
+
+import Mux from "@mux/mux-node";
+const { Video } = new Mux(
+  process.env.MUX_TOKEN_ID,
+  process.env.MUX_TOKEN_SECRET
+);
+
+export default async function handler(request, response) {
+  // Verify ?video= is present
+  if (!request.query.video) {
+    return response
+      .status(400)
+      .json({ error: "Missing video query parameter" });
+  }
+
+  const asset = await Video.Assets.create({
+    input: request.query.video,
+    playback_policy: "public",
+  });
+
+  // Return the asset ID
+  return response.json({ asset_id: asset.id });
 }
