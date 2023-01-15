b0a580aec94e1aa34f260e08d676d24426404902
Justin <88294592+dev-justin@users.noreply.github.com>
2023-01-15 12:19:38 -0500

    cc

--- api/mux.js
+++ api/mux.js
@@ -1,5 +1,4 @@
 // This is deployed as a serverless function on Vercel
 export default function handler(request, response) {
-  const { videoUrl } = request.query;
-  return response.end(`Video URL: ${videoUrl}`);
+  return response.json({ query: request.query, body: request.body });
 }
